104c06b6cdae300fed9a1d465968868a3395c756
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-26 22:30:25 +0300

    Added hello react

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  root to: 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
   
 namespace :api do
   namespace :v1 do
