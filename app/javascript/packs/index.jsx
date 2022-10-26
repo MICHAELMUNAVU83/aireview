bc70fe93df2d97c6e57671f72d96dec8be2d3ffd
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-26 22:52:14 +0300

    Added react router dom

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,21 +4,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 
-const Hello = (props) => <div>Hello {props.name}!</div>;
-
-Hello.defaultProps = {
-  name: "David",
-};
-
-Hello.propTypes = {
-  name: PropTypes.string,
-};
+import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="Reactf6g7hjo" />,
+    <App />,
     document.body.appendChild(document.createElement("div"))
   );
 });
