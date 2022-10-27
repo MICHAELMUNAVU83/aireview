d683cceb3aebafeb62cd8553487ae42b26400d5a
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-27 07:19:38 +0300

    Added components

--- app/javascript/components/Airlines.js
+++ app/javascript/components/Airlines.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Airlines() {
+  useEffect(() => {
+    fetch("https://mytecheventsapi.herokuapp.com/events")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  console.log("Airlines");
+
   return <div>Airlines</div>;
 }
 
